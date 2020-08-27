d705f595286be8849c25feac09c4f1e07f1bb3e1
HadravovaLucie <lucihadravova@gmail.com>
2020-08-27 12:18:18 +0200

    plus,minus

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,34 @@
-function clickCounterMinus() { 
-
+function clickCounterMinus(event) { 
+    let meal = event.target.className;
     if (typeof(Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount) - 1;
+
+       
+        if ( localStorage.getItem(meal)) {
+            let current = localStorage.getItem(meal);
+            localStorage.setItem(meal, Number(current) - 1);
         } else {
-            localStorage.clickcount = 1;
+            
         }
-        document.getElementById("result").innerHTML = localStorage.clickcount;
+        document.getElementById(meal).innerHTML = localStorage.getItem(meal);
     }
      else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        document.getElementById(meal).innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
 
-function clickCounterPlus() {
+function clickCounterPlus(event) {
+   let meal = event.target.className;
     if (typeof(Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount) + 1;
-        } else {
-            localStorage.clickcount = 1;
-        }
-        document.getElementById("result").innerHTML = localStorage.clickcount;
+
+       if(localStorage.getItem(meal)) {
+            let current = localStorage.getItem(meal);
+            localStorage.setItem(meal, Number(current) + 1);
+       } else {
+        localStorage.setItem(meal, 1);
+       }
+       
+        document.getElementById(meal).innerHTML = localStorage.getItem(meal);
     } else {
-        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        document.getElementById(meal).innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
